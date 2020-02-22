96fa8da4af4a84f7febbc1f7a94ed59b7c1139ba
Emma Ingram <eingram16@gmail.com>
2020-02-21 22:46:02 -0600

    Added dummy search bar and button for Search Gyms

--- screens/SearchGyms.js
+++ screens/SearchGyms.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { 
     StyleSheet, 
     View, 
-    Text, 
+    Text,
+    TextInput,
+    Button,
+    Alert 
 } from 'react-native';
 
 export default function SearchGym(){
+    const [value, onChangeText] = React.useState('Search Gym Here');
+
     return(
         <View style={styles.container}>
-            <Text>Search Gym Screen</Text>
+            <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={text => onChangeText(text)}
+                value={value}
+            />
+            <Button
+                title='Search'
+                color='#7fff00'
+                onPress={() => Alert.alert('Button pressed')}
+            />
         </View>
     );
 }
