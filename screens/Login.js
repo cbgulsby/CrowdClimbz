04fa1fd9d5df110f67c0e4912e03044afd38cd45
Will Harders <wjharders@crimson.ua.edu>
2020-03-08 23:15:58 -0500

    Altering Structure of Login Screen
    Changing into a pair of functions for consistency

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
     StyleSheet, 
     Text, 
@@ -6,62 +6,87 @@ import {
     Button, 
     TextInput 
 } from 'react-native';
+import firebase from 'firebase/app';
 
-//import firebase
+//import firebase from 'react-native-firebase';
+//import * as firebase from 'firebase';
 
-export default class Login extends React.Component {
-    state = {
-        email: '',
-        password: '',
-        errorMessage: null
-    }
+// firebase.initializeApp({
+// 	apiKey: "AIzaSyDfOuH54N5dKv5zqVZ3CylTZpn7y2eC9GI",
+//   	authDomain: "crowdclimbz.firebaseapp.com",
+//   	databaseURL: "https://crowdclimbz.firebaseio.com",
+//   	projectId: "crowdclimbz",
+//   	storageBucket: "crowdclimbz.appspot.com",
+//   	messagingSenderId: "760059550596",
+//   	appId: "1:760059550596:web:a88049a666755ee2ec1bd1",
+//   	measurementId: "G-3RRKPJLYNG"
+// });
 
-    loginAuthentication = () => {
-        // Firebase stuff
-    }
 
-    render() {
-        return (
-            <View style = { styles.container }>
-                <Text>Login</Text>
-                { /* Handle possible errors for login */ }
-                <TextInput
-                    style = { styles.textInput }
-                    autoCapitalize = 'none'
-                    placeholder = 'Email'
-                    onChangeText = { 
-                        (email) => this.setState({ email }) 
-                    }
-                    value = { this.state.email }
-                />
-                <TextInput
-                    secureTextEntry
-                    style = { styles.textInput }
-                    autoCapitalize = 'none'
-                    placeholder = 'Password'
-                    onChangeText = { 
-                        (password) => this.setState({ password })
-                    }
-                    value = { this.state.password }
-                />
-                <Button 
-                    title = 'Login'
-                    onPress = { 
-                        // () => this.loginAuthentication
-                        () => this.props.navigation.navigate('SideMenu')
-                    }
-                />
-                <Button
-                    title = "Don't have an account?"
-                    // Handle navigation to sign up screen
-                    onPress = { 
-                        () => this.props.navigation.navigate('SignUp') 
-                    }
-                />
-            </View>
-        )
-    }
+async function login(email, pass, navigation) {
+    
 }
+
+export default function Login({navigation}) {
+
+}
+
+
+
+
+// export default class Login extends React.Component {
+//     state = {
+//         email: '',
+//         password: '',
+//         errorMessage: null
+//     }
+
+//     loginAuthentication = () => {
+//         // Firebase stuff
+//     }
+
+//     render() {
+//         return (
+//             <View style = { styles.container }>
+//                 <Text>Login</Text>
+//                 { /* Handle possible errors for login */ }
+//                 <TextInput
+//                     style = { styles.textInput }
+//                     autoCapitalize = 'none'
+//                     placeholder = 'Email'
+//                     onChangeText = { 
+//                         (email) => this.setState({ email }) 
+//                     }
+//                     value = { this.state.email }
+//                 />
+//                 <TextInput
+//                     secureTextEntry
+//                     style = { styles.textInput }
+//                     autoCapitalize = 'none'
+//                     placeholder = 'Password'
+//                     onChangeText = { 
+//                         (password) => this.setState({ password })
+//                     }
+//                     value = { this.state.password }
+//                 />
+//                 <Button 
+//                     title = 'Login'
+//                     onPress = { 
+//                         // () => this.loginAuthentication
+//                         () => this.props.navigation.navigate('SideMenu')
+//                     }
+//                 />
+//                 <Button
+//                     title = "Don't have an account?"
+//                     // Handle navigation to sign up screen
+//                     onPress = { 
+//                         () => this.props.navigation.navigate('SignUp') 
+//                     }
+//                 />
+//             </View>
+//         )
+//     }
+// }
 
 const styles = StyleSheet.create({
     container: {
