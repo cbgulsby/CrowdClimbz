94d3aa0ee34f64c55dac7f775029c532af096278
Emma Ingram <eingram16@gmail.com>
2020-04-01 16:33:42 -0500

    Connected changepassword to firestore test

--- screens/ChangePassword.js
+++ screens/ChangePassword.js
@@ -7,10 +7,24 @@ import {
     TouchableOpacity,
     Button,
     TextInput,
+    Alert,
 } from 'react-native';
+//import * as firebase from 'firebase/app';
+//import 'firebase/firestore';
+import firebase from '../firebase';
 
-export default function ChangePassword({navigation}){
-    
+
+
+export default function ChangePassword({navigation}){   
+    function send(val) {
+        var dbh = firebase.firestore();
+
+        dbh.collection('gyms').doc('testgym').update({name: val});
+        navigation.navigate('Profile');
+        Alert.alert("Password Changed");
+
+    }
+
     const [value, onChangeText] = React.useState('Change Password');
     return(
         <SafeAreaView style={styles.container}>
@@ -20,6 +34,7 @@ export default function ChangePassword({navigation}){
                     onChangeText={text => onChangeText(text)}
                     value={value}
                 />
+                <Button  title="Submit Changes" onPress={() => send(value)} />
 
                 <Button title="Go Back" onPress={() => navigation.navigate('Profile')} />
             </View>
