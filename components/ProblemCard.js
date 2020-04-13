32188abe2b7f8b6ac16d8d23730e87f4011e5236
Will Harders <wjharders@crimson.ua.edu>
2020-04-13 16:28:31 -0500

    Test Commit
    Testing commit with new branch by adding navigation to ProblemCard component

--- components/ProblemCard.js
+++ components/ProblemCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function ProblemCard(props) {
+export default function ProblemCard(props, {navigation}) {
     return (
         <TouchableOpacity style={styles.container}>
             <View style={styles.leftInnerContainer}>
