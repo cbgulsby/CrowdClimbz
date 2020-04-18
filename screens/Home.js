bd006562167328634b49a8cbb9dbb436eebda8b1
Bailey <cbgulsby@crimson.ua.edu>
2020-04-18 18:36:52 -0500

    Updated Problem Components and Screens

--- screens/Home.js
+++ screens/Home.js
@@ -6,7 +6,7 @@ import {
   StyleSheet, 
   FlatList
 } from 'react-native';
-import ProblemCard from '../components/ProblemCard';
+import ProblemList from '../components/ProblemList';
 import firebase from '../firebase';
 console.disableYellowBox = true;
 
@@ -19,19 +19,35 @@ export default function Home({navigation}) {
       const tempProblems = [];
       querySnapshot.forEach(doc => {
         const{
+          betaVideo,
+          date,
+          description,
           grade,
           gym,
+          inappropriateFlag,
           name,
+          outOfDateFlag,
+          photo,
+          time,
           user
         } = doc.data();
 
+        // tempProblems.push({
+        //   gym: gym,
+        //   user: user,
+        //   grade: grade,
+        //   name: name,
+        //   id: doc.id
+        // });
         tempProblems.push({
-          gym: gym,
-          user: user,
-          grade: grade,
-          name: name,
-          id: doc.id
-        });
+          problemInfo: {
+              gymName: gym,
+              user: user,
+              grade: grade,
+              problemName: name,
+          },
+          key: doc.id
+      })
       });
       setProblems(tempProblems);
     });
@@ -40,13 +56,14 @@ export default function Home({navigation}) {
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 30, color: 'white'}}>Welcome to CrowdClimbz!</Text>
-      <FlatList
+      <Text style={{fontSize: 30, color: '#073B4C', fontWeight: 'bold'}}>Welcome to CrowdClimbz!</Text>
+      {/* <FlatList
          style={{width: 350}}
          data={problems}
          renderItem={({item}) => <ProblemCard id={item.id} problemName={item.name} gymLocation={item.gym} problemLevel={item.grade} />} 
          keyExtractor={item => item.id} 
-      />
+      /> */}
+      <ProblemList problems={problems} />
     </View>
   );
 }
@@ -54,7 +71,7 @@ export default function Home({navigation}) {
 const styles = StyleSheet.create({
   container: {
       paddingTop: 24,
-      backgroundColor: '#4fb9ff',
+      backgroundColor: '#118AB2',
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center'
